6e26c95d164a812bad84630c56dc97d9a6db76fe
ocnasc <ocnasc@gmail.com>
2024-08-14 11:38:19 -0300

    feat: add folder structure

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,15 +1,15 @@
 import { Router } from 'express';
-
-
+import { StatusCodes } from 'http-status-codes';
 
 const router = Router();
 
 router.get('/', (req, res) => {
-  return res.send('API Funcionando!');
+  return res.status(StatusCodes.OK).send('API Funcionando!');
 });
+
 router.post('/teste', (req, res) => {
   console.log(req.body);
-  return res.status(418).send('hey');
+  return res.status(StatusCodes.OK).send('Teste POST');
 });
 
 
