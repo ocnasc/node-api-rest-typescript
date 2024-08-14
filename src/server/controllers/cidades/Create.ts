294bd2fc63c3d0ea698bbacf43e7a645ff0ae6be
ocnasc <ocnasc@gmail.com>
2024-08-14 17:52:14 -0300

    update

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,10 +1,37 @@
 import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import * as yup from 'yup';
 
 interface ICidade {
   nome: string;
+  estado: string
 }
 
-export const create = (req: Request<{}, {}, ICidade>, res: Response) => {
-  console.log(req.body);
-  return res.send("Create!");
+const bodyValidation: yup.ObjectSchema<ICidade> = yup.object().shape({
+  nome: yup.string().required().min(3),
+  estado: yup.string().required().min(3),
+
+});
+
+export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+let validatedData: ICidade | undefined = undefined;
+
+  try {
+    validatedData = await bodyValidation.validate(req.body, {abortEarly: false});
+  } catch (error) {
+    const yupError = error as yup.ValidationError;
+    const validationErrors: Record<string,string> = {};
+
+    yupError.inner.forEach(error => {
+      if (error.path === undefined) return;
+      validationErrors[error.path] = error.message;
+    })
+    console.log({statusCode: StatusCodes.BAD_REQUEST,user_request: req.body, errors: validationErrors})
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      errors: validationErrors,
+    })
+  }
+
+  console.log(validatedData);
+  return res.status(StatusCodes.OK).send("tudo certo!");
 };
